15a2555fb03c9ad41351981bd949b0dd6bebb55a
bizzard _ <stefanwitchell1990@gmail.com>
2019-12-28 23:16:04 +0000

    End of section 16

--- DatingApp-SPA/src/app/_services/user.service.ts
+++ DatingApp-SPA/src/app/_services/user.service.ts
@@ -1,13 +1,14 @@
-import { Injectable } from "@angular/core";
-import { environment } from "src/environments/environment";
-import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
-import { Observable } from "rxjs";
-import { User } from "../_models/user";
-import { PaginatedResult } from "../_models/pagination";
-import { map } from "rxjs/operators";
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from '../_models/user';
+import { PaginatedResult } from '../_models/pagination';
+import { map } from 'rxjs/operators';
+import { Message } from '../_models/message';
 
 @Injectable({
-  providedIn: "root"
+  providedIn: 'root'
 })
 export class UserService {
   baseUrl = environment.apiUrl;
@@ -27,33 +28,33 @@ export class UserService {
     let params = new HttpParams();
 
     if (page != null && itemsPerPage != null) {
-      params = params.append("pageNumber", page);
-      params = params.append("pageSize", itemsPerPage);
+      params = params.append('pageNumber', page);
+      params = params.append('pageSize', itemsPerPage);
     }
 
     if (userParams != null) {
-      params = params.append("minAge", userParams.minAge);
-      params = params.append("maxAge", userParams.maxAge);
-      params = params.append("gender", userParams.gender);
-      params = params.append("orderBy", userParams.orderBy);
+      params = params.append('minAge', userParams.minAge);
+      params = params.append('maxAge', userParams.maxAge);
+      params = params.append('gender', userParams.gender);
+      params = params.append('orderBy', userParams.orderBy);
     }
 
-    if (likesParam === "Likers") {
-      params = params.append("likers", "true");
+    if (likesParam === 'Likers') {
+      params = params.append('likers', 'true');
     }
 
-    if (likesParam === "Likees") {
-      params = params.append("likees", "true");
+    if (likesParam === 'Likees') {
+      params = params.append('likees', 'true');
     }
 
     return this.http
-      .get<User[]>(this.baseUrl + "users", { observe: "response", params })
+      .get<User[]>(this.baseUrl + 'users', { observe: 'response', params })
       .pipe(
         map(response => {
           paginatedResult.result = response.body;
-          if (response.headers.get("Pagination") != null) {
+          if (response.headers.get('Pagination') != null) {
             paginatedResult.pagination = JSON.parse(
-              response.headers.get("Pagination")
+              response.headers.get('Pagination')
             );
           }
           return paginatedResult;
@@ -62,28 +63,70 @@ export class UserService {
   }
 
   getUser(id): Observable<User> {
-    return this.http.get<User>(this.baseUrl + "users/" + id);
+    return this.http.get<User>(this.baseUrl + 'users/' + id);
   }
 
   updateUser(id: number, user: User) {
-    return this.http.put(this.baseUrl + "users/" + id, user);
+    return this.http.put(this.baseUrl + 'users/' + id, user);
   }
 
   setMainPhoto(userId: number, id: number) {
     return this.http.post(
-      this.baseUrl + "users/" + userId + "/photos/" + id + "/setMain",
+      this.baseUrl + 'users/' + userId + '/photos/' + id + '/setMain',
       {}
     );
   }
 
   deletePhoto(userId: number, id: number) {
-    return this.http.delete(this.baseUrl + "users/" + userId + "/photos/" + id);
+    return this.http.delete(this.baseUrl + 'users/' + userId + '/photos/' + id);
   }
 
   sendLike(id: number, recipientId: number) {
     return this.http.post(
-      this.baseUrl + "users/" + id + "/like/" + recipientId,
+      this.baseUrl + 'users/' + id + '/like/' + recipientId,
       {}
     );
+  }
+
+  getMessages(id: number, page?, itemsPerPage?, messageContainer?) {
+    const paginatedResult: PaginatedResult<Message[]> = new PaginatedResult<Message[]>();
+
+    let params = new HttpParams();
+
+    params = params.append('MessageContainer', messageContainer);
+
+    if (page != null && itemsPerPage != null) {
+      params = params.append('pageNumber', page);
+      params = params.append('pageSize', itemsPerPage);
+    }
+
+    return this.http.get<Message[]>(this.baseUrl + 'users/' + id + '/messages', {observe: 'response', params})
+      .pipe(
+        map(response => {
+          paginatedResult.result = response.body;
+          if (response.headers.get('Pagination') !== null) {
+            paginatedResult.pagination = JSON.parse(response.headers.get('Pagination'));
+          }
+
+          return paginatedResult;
+        })
+      );
+  }
+
+  getMessageThread(id: number, recipientId: number) {
+    return this.http.get<Message[]>(this.baseUrl + 'users/' + id + '/messages/thread/' + recipientId);
+  }
+
+  sendMessage(id: number, message: Message) {
+    return this.http.post(this.baseUrl + 'users/' + id + '/messages', message);
+  }
+
+  deleteMessage(id: number, userId: number) {
+    return this.http.post(this.baseUrl + 'users/' + userId + '/messages/' + id, {});
+  }
+
+  markAsRead(userId: number, messageId: number) {
+    this.http.post(this.baseUrl + 'users/' + userId + '/messages/' + messageId + '/read', {})
+      .subscribe();
   }
 }
--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -2,49 +2,51 @@ import {
   BrowserModule,
   HammerGestureConfig,
   HAMMER_GESTURE_CONFIG
-} from "@angular/platform-browser";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { NgModule } from "@angular/core";
-import { HttpClientModule } from "@angular/common/http";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+} from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import {
   BsDropdownModule,
   TabsModule,
   BsDatepickerModule,
   PaginationModule,
   ButtonsModule
-} from "ngx-bootstrap";
-import { RouterModule } from "@angular/router";
-import { JwtModule } from "@auth0/angular-jwt";
-import { NgxGalleryModule } from "ngx-gallery";
-import { FileUploadModule } from "ng2-file-upload";
-import { TimeAgoPipe } from "time-ago-pipe";
+} from 'ngx-bootstrap';
+import { RouterModule } from '@angular/router';
+import { JwtModule } from '@auth0/angular-jwt';
+import { NgxGalleryModule } from 'ngx-gallery';
+import { FileUploadModule } from 'ng2-file-upload';
+import { TimeAgoPipe } from 'time-ago-pipe';
 
-import { AppComponent } from "./app.component";
-import { NavComponent } from "./nav/nav.component";
-import { AuthService } from "./_services/auth.service";
-import { HomeComponent } from "./home/home.component";
-import { RegisterComponent } from "./register/register.component";
-import { ErrorInterceptorProvider } from "./_services/error.interceptor";
-import { AlertifyService } from "./_services/alertify.service";
-import { MemberListComponent } from "./members/member-list/member-list.component";
-import { ListsComponent } from "./lists/lists.component";
-import { MessagesComponent } from "./messages/messages.component";
-import { appRoutes } from "./routes";
-import { AuthGuard } from "./_guards/auth.guard";
-import { UserService } from "./_services/user.service";
-import { MemberCardComponent } from "./members/member-card/member-card.component";
-import { MemberDetailComponent } from "./members/member-detail/member-detail.component";
-import { MemberDetailResolver } from "./_resolvers/member-detail.resolver";
-import { MemberListResolver } from "./_resolvers/member-list.resolver";
-import { MemberEditComponent } from "./members/member-edit/member-edit.component";
-import { MemberEditResolver } from "./_resolvers/member-edit.resolver";
-import { PreventUnsavedChanges } from "./_guards/prevent-unsaved-changes.guard";
-import { PhotoEditorComponent } from "./members/photo-editor/photo-editor.component";
-import { ListsResolver } from "./_resolvers/lists.resolver";
+import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { AuthService } from './_services/auth.service';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
+import { ErrorInterceptorProvider } from './_services/error.interceptor';
+import { AlertifyService } from './_services/alertify.service';
+import { MemberListComponent } from './members/member-list/member-list.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
+import { appRoutes } from './routes';
+import { AuthGuard } from './_guards/auth.guard';
+import { UserService } from './_services/user.service';
+import { MemberCardComponent } from './members/member-card/member-card.component';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { MemberDetailResolver } from './_resolvers/member-detail.resolver';
+import { MemberListResolver } from './_resolvers/member-list.resolver';
+import { MemberEditComponent } from './members/member-edit/member-edit.component';
+import { MemberEditResolver } from './_resolvers/member-edit.resolver';
+import { PreventUnsavedChanges } from './_guards/prevent-unsaved-changes.guard';
+import { PhotoEditorComponent } from './members/photo-editor/photo-editor.component';
+import { ListsResolver } from './_resolvers/lists.resolver';
+import { MessagesResolver } from './_resolvers/messages.resolver';
+import { MemberMessagesComponent } from './members/member-messages/member-messages.component';
 
 export function tokenGetter() {
-  return localStorage.getItem("token");
+  return localStorage.getItem('token');
 }
 
 export class CustomHammerConfig extends HammerGestureConfig {
@@ -67,7 +69,8 @@ export class CustomHammerConfig extends HammerGestureConfig {
     MemberDetailComponent,
     MemberEditComponent,
     PhotoEditorComponent,
-    TimeAgoPipe
+    TimeAgoPipe,
+    MemberMessagesComponent
   ],
   imports: [
     BrowserModule,
@@ -86,8 +89,8 @@ export class CustomHammerConfig extends HammerGestureConfig {
     JwtModule.forRoot({
       config: {
         tokenGetter,
-        whitelistedDomains: ["localhost:5000"],
-        blacklistedRoutes: ["localhost:5000/api/auth"]
+        whitelistedDomains: ['localhost:5000'],
+        blacklistedRoutes: ['localhost:5000/api/auth']
       }
     })
   ],
@@ -102,7 +105,8 @@ export class CustomHammerConfig extends HammerGestureConfig {
     { provide: HAMMER_GESTURE_CONFIG, useClass: CustomHammerConfig },
     MemberEditResolver,
     PreventUnsavedChanges,
-    ListsResolver
+    ListsResolver,
+    MessagesResolver
   ],
   bootstrap: [AppComponent]
 })
